591691c3364e86baea46dc8152852541a41716ea
Anton Cherepanov <davetoxa@gmail.com>
2018-09-02 13:22:20 +0500

    Simple view

--- cards/urls.py
+++ cards/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'cards'
+
 urlpatterns = [
+    # ex: /cards/
     path('', views.index, name='index'),
+    # ex: /cards/5/
+    path('<int:card_id>/', views.detail, name='detail'),
 ]
--- cards/views.py
+++ cards/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
+from .models import Card
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_card_list = Card.objects.order_by('-id')[:5]
+
+    context = {'latest_card_list': latest_card_list}
+    return render(request, 'cards/index.html', context)
+
+def detail(request, card_id):
+    card = get_object_or_404(Card, pk=card_id)
+    return render(request, 'cards/detail.html', {'card': card})
+
+# def check(request, card_id):
+#     response = "You're checking the card %s."
+#     return HttpResponse(response % card_id)
